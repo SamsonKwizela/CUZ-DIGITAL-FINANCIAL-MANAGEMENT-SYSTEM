0619712bac567038af7f3d6b621519224dda342d
Desire <desireirankundawilliams@gmail.com>
2025-11-05 00:36:12 +0200

    cleaned the code

--- cuz-dfms/src/App.js
+++ cuz-dfms/src/App.js
@@ -1,49 +1,34 @@
-
-import './App.css';
-import { MantineProvider } from '@mantine/core';
-import { Login } from './auth/Login';
-import { Hero } from './landingPage/Hero';
+import "./App.css";
+import { MantineProvider } from "@mantine/core";
+import { Login } from "./auth/Login";
+import { Hero } from "./landingPage/Hero";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navigation } from './component/NavBar';
+import { Navigation } from "./component/NavBar";
 import ContactUs from "./landingPage/contactUs";
-import { ForgotPassword } from './auth/ForgotPassword';
-import { ChooseAccountType } from './register/ChooseAccountType';
-import { StudentAccountRegister } from './register/StudentAccountRegister';
-import { Overview } from './dashBoard/overview';
-import AuthStepper from './auth/AuthStepper';
-
-
+import { ForgotPassword } from "./auth/ForgotPassword";
+import { ChooseAccountType } from "./register/ChooseAccountType";
+import { StudentAccountRegister } from "./register/StudentAccountRegister";
+import AuthStepper from "./auth/AuthStepper";
+import { Dashboard } from "./dashBoard/index.jsx";
 
 function App() {
-
-
-
   return (
-    
-  <MantineProvider>
-    <Navigation />
-    <Router>
-      <Routes>
-        <Route path = "/" element={<Hero />} />
-        <Route path = "/login" element={<Login />} />
-        <Route path = "/contact" element={<ContactUs />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/choose-account" element={<ChooseAccountType />} />
-        <Route path="/register" element={<AuthStepper />} />
-        <Route path="/overview" element={<Overview />} />
-         <Route path="/contact" element={<ContactUs />} />
-
-
-      </Routes>
-    </Router>
-      
-
-
-    
-    
-  </MantineProvider>
-
+    <MantineProvider>
+      <Navigation />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/choose-account" element={<ChooseAccountType />} />
+          <Route path="/register" element={<AuthStepper />} />
+          <Route path="/overview" element={<Dashboard />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </Router>
+    </MantineProvider>
   );
 }
 
-export default App; 
+export default App;
