5c1b6d3a59b63ca00c3c9a13806ebd105d539e8c
Desire <desireirankundawilliams@gmail.com>
2025-11-06 10:15:11 +0200

    cleaned the code

--- src/dashBoard/Balance.js
+++ src/dashBoard/Balance.js
@@ -14,7 +14,7 @@ import { IconRefresh, IconWallet } from "@tabler/icons-react";
 import moment from "moment";
 import { accountBalance } from "../services/authService";
 import Loading from "../component/Loading";
-import { formatAmount } from "../utils/schemaValidation/src/utils/src/utils/Helpers";
+import { formatAmount } from "../utils/schemaValidation/Helpers";
 import { toast } from "react-toastify";
 
 const Balance = () => {
