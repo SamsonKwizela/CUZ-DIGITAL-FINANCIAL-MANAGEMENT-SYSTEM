2017f1de3d16b0a3ad2ed3af7e30b910ba6bb761
Desire <desireirankundawilliams@gmail.com>
2025-11-06 14:58:09 +0200

    cleaned the code

--- src/component/ReusableTable.js
+++ src/component/ReusableTable.js
@@ -3,8 +3,9 @@ import { formatAmount } from "../schemaValidation/Helpers";
 import moment from "moment";
 
 function ReusableTable({ transaction }) {
-  const rows = transaction?.map((transaction) => (
+  const rows = transaction?.map((transaction, index) => (
     <Table.Tr key={transaction.id}>
+      <Table.Td>{index + 1}</Table.Td>
       <Table.Td>{transaction?.from?.accountHolderName}</Table.Td>
       <Table.Td>{transaction?.to?.accountHolderName}</Table.Td>
       <Table.Td>{transaction?.from?.accountNumber}</Table.Td>
@@ -22,6 +23,7 @@ function ReusableTable({ transaction }) {
     <Table>
       <Table.Thead>
         <Table.Tr>
+          <Table.Th>No</Table.Th>
           <Table.Th>From</Table.Th>
           <Table.Th>To</Table.Th>
           <Table.Th>Account number</Table.Th>
